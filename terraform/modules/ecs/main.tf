ebac73ca7b01014847fb12236fa69887a6e5d146
Raul Robledo <ra_o_robledo@mcafee.com>
2020-09-15 16:24:23 -0300

    Apply patch to deploy on aws

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -27,6 +27,38 @@ resource "aws_ecr_repository" "challenge_repo" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_repository_policy" "challenge-repo-policy" {
+  repository = aws_ecr_repository.challenge_repo.name
+
+  policy = <<EOF
+            {
+                "Version": "2008-10-17",
+                "Statement": [
+                    {
+                        "Sid": "new policy",
+                        "Effect": "Allow",
+                        "Principal": "*",
+                        "Action": [
+                            "ecr:GetDownloadUrlForLayer",
+                            "ecr:BatchGetImage",
+                            "ecr:BatchCheckLayerAvailability",
+                            "ecr:PutImage",
+                            "ecr:InitiateLayerUpload",
+                            "ecr:UploadLayerPart",
+                            "ecr:CompleteLayerUpload",
+                            "ecr:DescribeRepositories",
+                            "ecr:GetRepositoryPolicy",
+                            "ecr:ListImages",
+                            "ecr:DeleteRepository",
+                            "ecr:BatchDeleteImage",
+                            "ecr:SetRepositoryPolicy",
+                            "ecr:DeleteRepositoryPolicy"
+                        ]
+                    }
+                ]
+            }
+            EOF
+}
 
 resource "aws_ecs_cluster" "cluster" {
   name = var.cluster
