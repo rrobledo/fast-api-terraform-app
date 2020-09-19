3281c6f91ac1b4d7fe321fed1c1437fd9eeed8e2
Raul Robledo <ra_o_robledo@mcafee.com>
2020-09-19 00:46:49 -0300

    fixing terraform format issue

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  app_name          = "challenge"
+  app_name = "challenge"
   container_definition = jsonencode({
-    "name"        = "${local.app_name}-${var.environment}"
-    "image"       = "${module.ecs.aws_ecr_repository.repository_url}:${var.IMAGE_TAG}"
-    "essential"   = true
+    "name"      = "${local.app_name}-${var.environment}"
+    "image"     = "${module.ecs.aws_ecr_repository.repository_url}:${var.IMAGE_TAG}"
+    "essential" = true
     "portMappings" = [{
       hostPort      = 80,
       protocol      = "tcp",
